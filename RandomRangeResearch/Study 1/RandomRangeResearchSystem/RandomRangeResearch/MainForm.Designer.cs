4cf7965bab8767b9efb444c29c14866e9ec81c89
Ćujić Siniša <101299300+cujics@users.noreply.github.com>
2022-06-09 01:21:56 +0200

    Add files via upload

---
+++ RandomRangeResearch/Study 1/RandomRangeResearchSystem/RandomRangeResearch/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TS
+ * Date: 6.6.2022.
+ * Time: 12:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace RandomRangeResearch
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.label4 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button1.Location = new System.Drawing.Point(287, 36);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Изврши";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(90, 36);
+			this.trackBar1.Minimum = 1;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(185, 45);
+			this.trackBar1.TabIndex = 1;
+			this.trackBar1.Value = 1;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(12, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(72, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Подеоци:";
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(27, 134);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(206, 173);
+			this.listBox1.TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(103, 88);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(70, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(280, 88);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(70, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label2.Location = new System.Drawing.Point(13, 90);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(88, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Минимални:";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label3.Location = new System.Drawing.Point(183, 91);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(92, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Максимални:";
+			// 
+			// listBox2
+			// 
+			this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.Location = new System.Drawing.Point(239, 134);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(123, 173);
+			this.listBox2.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+			this.label4.Location = new System.Drawing.Point(27, 9);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(323, 23);
+			this.label4.TabIndex = 9;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(384, 329);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.listBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "RandomRangeResearch";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ RandomRangeResearch/Study 1/RandomRangeResearchSystem/RandomRangeResearch/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RandomRangeResearch
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		int Case_Q_Index(int min,int max,double Q_Index)
+		{
+			return min+Convert.ToInt32(Math.Round((max-min)*Q_Index));
+		}
+		void Show_Case(int min,int max,int mQ_Index)
+		{
+			if(File.Exists("raw_data.txt"))File.Delete("raw_data.txt");
+			StreamWriter To_File = new StreamWriter("raw_data.txt");
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();
+			Dictionary<int,int> map = new Dictionary<int, int>();
+			for(int i=0;i<1000*mQ_Index+1;i++){
+				double Q_Index = (i/1000.0)/mQ_Index;
+				int result =Case_Q_Index(min,max,Q_Index);
+				if(map.ContainsKey(result))map[result]++;
+				else map[result]=1;
+				string temp = Q_Index.ToString() + ":" + result.ToString();
+				To_File.WriteLine(temp);
+				listBox1.Items.Add(temp);
+			}
+			To_File.Close();
+			if(File.Exists("raw_data2.txt"))File.Delete("raw_data2.txt");
+			To_File = new StreamWriter("raw_data2.txt");
+			foreach(KeyValuePair<int,int> pair in map){
+				string temp = pair.Key.ToString() + ":" + pair.Value.ToString();
+				listBox2.Items.Add(temp);
+				To_File.WriteLine(temp);
+			}
+			To_File.Close();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			try{Show_Case(Convert.ToInt32(textBox1.Text),Convert.ToInt32(textBox2.Text),trackBar1.Value);}
+			catch{}
+		}
+	}
+}
